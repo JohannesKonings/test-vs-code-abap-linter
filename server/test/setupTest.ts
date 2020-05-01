5c1a15938f5678d4e841be0b15e33e9e51d6f3be
Johannes Konings <johannes.konings@outlook.com>
2020-05-01 13:01:17 +0200

    tests added

--- server/test/setupTest.ts
+++ server/test/setupTest.ts
@@ -1,12 +1,28 @@
 import { expect } from 'chai';
-import { Setup } from "./../src/setup";
+import { createConnection, IConnection, IPCMessageReader, IPCMessageWriter, WorkspaceFolder } from 'vscode-languageserver';
 import { IFolder } from "./../src/handler";
-import {
-    IPCMessageReader, IPCMessageWriter, createConnection, IConnection
-} from 'vscode-languageserver';
+import { Setup } from "./../src/setup";
 
-describe("read config", () => {
-    let ret: IFolder[] = []; //["${workspaceFolder}/server/test/"];
+describe("determine folders", () => {
+    const connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+    let setup = new Setup(connection);
+    let ws: WorkspaceFolder[] = [{uri: "dummy", name: "name"}];
+    const ret = setup.determineFolders(ws);
+    console.log(ret);
+    it('should return deps files', () => { expect(1).to.equals(1); });
+});
+
+describe("read config with Files", () => {
+    let ret: IFolder[] = [{root: "workspace.getWorkspaceFolder.toString()", glob: "name"}];
+    console.log(ret);
+    const connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+    let setup = new Setup(connection);
+    setup.readConfig(ret);
+    it('should return deps files', () => { expect(1).to.equals(1); });
+});
+
+describe("read config without Files", () => {
+    let ret: IFolder[] = [];
     console.log(ret);
     const connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
     let setup = new Setup(connection);
